123101400d6cec78ed92b89ac6003046c4c4e65c
Veli <109654472+velielagoz10@users.noreply.github.com>
2023-10-19 14:59:51 +0300

    Add files via upload

--- css/JavaScript.js
+++ css/JavaScript.js
@@ -14,3 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+/*......................  Yukarı ok tuşu Kodu  .................. */
+function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+window.onscroll = function () {
+  var scrollButton = document.getElementById("goTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+};
