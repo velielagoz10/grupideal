346b0d006050015743b7398de98ef5e98acf496b
Veli <109654472+velielagoz10@users.noreply.github.com>
2023-10-25 10:28:28 +0300

    Add files via upload

---
+++ bootstrap/css/JavaScript.js
@@ -0,0 +1,42 @@
+// ..................... bekletme kodu........................
+// JavaScript kodunuzu burada ekleyin
+document.addEventListener("DOMContentLoaded", function () {
+  // Tüm linkleri seçin
+  var links = document.querySelectorAll("a");
+
+  // Her bir link için işlem yapın
+  links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      setTimeout(function () {
+        window.location.href = link.href;
+      }, 1000);
+    });
+  });
+});
+/*......................  Yukarı ok tuşu Kodu  .................. */
+function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+window.onscroll = function () {
+  var scrollButton = document.getElementById("goTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+};
+
+/*......................  Bayiler  .................. */
+
+$(document).ready(function () {
+  $("#filterInput").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("table tbody tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
